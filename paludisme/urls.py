de9be55687dfad6339401d62cdd8c161822dbe65
RUGANO Allan Stockman <kuristockyrugano@gmail.com>
2017-03-21 11:56:57 +0200

    Added logout redirect

--- paludisme/urls.py
+++ paludisme/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     url(r'^$', home, name='home'),
     url(r'^admin/', admin.site.urls),
     url(r'^login/$', auth_views.login, name='login'),
-    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^oauth/', include('social_django.urls', namespace='social')),
 ]
