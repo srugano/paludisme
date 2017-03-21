9846ca99be3d22a77a1cb30ef507d0b4e61ec772
RUGANO Allan Stockman <kuristockyrugano@gmail.com>
2017-03-21 13:14:06 +0200

    added settings and password config page

--- paludisme/urls.py
+++ paludisme/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from paludisme.views import home
+from paludisme.views import home, settings, password
 
 
 urlpatterns = [
@@ -10,4 +10,6 @@ urlpatterns = [
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^oauth/', include('social_django.urls', namespace='social')),
+    url(r'^settings/$', settings, name='settings'),
+    url(r'^settings/password/$', password, name='password'),
 ]
