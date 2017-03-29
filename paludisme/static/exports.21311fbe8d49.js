6744ccf5fb522f9604829686c2c593aca0823a4f
RUGANO Allan Stockman <kuristockyrugano@gmail.com>
2017-03-29 10:08:58 +0200

    added some statics

---
+++ paludisme/static/exports.21311fbe8d49.js
@@ -0,0 +1,11 @@
+var app = angular.module('RepportApp', [ 'datatables', 'datatables.buttons']);
+
+app.controller('RepportCtrl', ['$scope', '$http', 'DTOptionsBuilder', function($scope, $http, DTOptionsBuilder) {
+    // for datatable 
+      $scope.dtOptions = DTOptionsBuilder.newOptions().withPaginationType('full_numbers').withButtons([ 'copy', 'csv', 'excel', 'pdf', 'print']).withDOM("<'row'<'col-sm-3'l><'col-sm-4'i><'col-sm-5'f>>" + "<'row'<'col-sm-12'tr>>" + "<'row'<'col-sm-4'B><'col-sm-8'p>>").withDisplayLength(10);
+      // province
+        $http.get("/stock/")
+        .then(function (response) {
+          $scope.reports = response.data.reports;
+      });
+}]);
