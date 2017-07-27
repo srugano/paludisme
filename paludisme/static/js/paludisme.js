3c0200b82777acdd949d3d284dbace52debf7bc5
RUGANO Allan Stockman <kuristockyrugano@gmail.com>
2017-07-27 12:55:10 +0200

    angular component

---
+++ paludisme/static/js/paludisme.js
@@ -0,0 +1,51 @@
+var app = angular.module('StockApp', ['ngSanitize', 'datatables', 'datatables.buttons']);
+
+app.controller('Filter', ['$scope', '$http', function($scope, $http) {
+
+        // province
+        $http.get("/bdiadmin/province/")
+        .then(function (response) {
+            if (response.data.length > 0) {
+                $scope.provinces = response.data;
+            } else {
+                $("#province-group").hide();
+                $http.get("/bdiadmin/district/")
+                .then(function (response) {
+                    $scope.districts = response.data;
+                });
+            }
+        });
+        $scope.update_province = function () {
+            var province = $scope.province;
+            if (province) {
+              $http.get("/bdiadmin/district/?province=" + province.id)
+              .then(function (response) {
+                $scope.districts = response.data;
+            });
+          }
+      };
+          // district
+          $scope.update_district = function () {
+            var district = $scope.district;
+            console.log(district);
+            if (district) {
+              $http.get("/bdiadmin/cds/?district=" + district.id)
+              .then(function (response) {
+                  $scope.cdss = response.data;
+              });
+          }
+      };
+        // CDS
+        $scope.update_cds = function () {
+            var cds = $scope.cds;
+            if (cds) {
+              $http.get("/bdiadmin/cds/" + cds.id + "/" )
+              .then(function (response) {
+                  $scope.etablissements = response.data;
+              });
+      }
+    };
+  }]);
+
+app.controller('ExportCtrl', ['$scope', '$http', 'DTOptionsBuilder', function($scope, $http, DTOptionsBuilder) {$scope.dtOptions = DTOptionsBuilder.newOptions().withPaginationType('full_numbers').withButtons([ 'copy', 'csv', 'excel', 'pdf', 'print']).withDOM("<'row'<'col-sm-3'l><'col-sm-4'i><'col-sm-5'f>>" + "<'row'<'col-sm-12'tr>>" + "<'row'<'col-sm-4'B><'col-sm-8'p>>").withDisplayLength(10);
+  }]);
