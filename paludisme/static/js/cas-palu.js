42f5a570de44df367490680c458e12e3d688eccc
RUGANO Allan Stockman <kuristockyrugano@gmail.com>
2017-08-08 15:25:01 +0200

    resolved updating facilities

--- paludisme/static/js/cas-palu.js
+++ paludisme/static/js/cas-palu.js
@@ -1,9 +1,8 @@
 var app = angular.module('PaludismeApp', ['ngSanitize', 'datatables', 'datatables.buttons']);
 
 app.controller('FilterCtrl', ['$scope', '$http', 'DTOptionsBuilder',  function($scope, $http, DTOptionsBuilder) {
-
-        // for export
-        $scope.dtOptions = DTOptionsBuilder.newOptions().withPaginationType('full_numbers').withButtons([ 'copy', 'csv', 'excel', 'pdf', 'print']).withDOM("<'row'<'col-sm-3'l><'col-sm-4'i><'col-sm-5'f>>" + "<'row'<'col-sm-12'tr>>" + "<'row'<'col-sm-4'B><'col-sm-8'p>>").withDisplayLength(10);
+        $scope.districtss = false;
+        $scope.cdsss = false;
         // province
         $http.get("/bdiadmin/province/")
         .then(function (response) {
@@ -26,13 +25,15 @@ app.controller('FilterCtrl', ['$scope', '$http', 'DTOptionsBuilder',  function($
               $http.get("/bdiadmin/district/?province=" + province.id)
                 .then(function (response) {
                   $scope.districts = response.data;
+                  $scope.cdss = "";
               });
               $http.get("/stock/casespalusProv/?report__facility__district__province=" + province.id)
               .then(function (response) {
+                  $scope.districtss = false;
+                  $scope.cdsss = false;
                   if (response.data.length > 0) {
+                  console.log($scope.cdsss, $scope.districtss);
                   $scope.structures = response.data;
-                  $scope.districtss = null;
-                  $scope.cdsss = null;
                   }
               });
           }
@@ -40,15 +41,15 @@ app.controller('FilterCtrl', ['$scope', '$http', 'DTOptionsBuilder',  function($
           // district
           $scope.update_district = function () {
             var district = $scope.district;
+            $scope.districtss = true;
             if (district) {
               $http.get("/bdiadmin/cds/?district=" + district.id)
               .then(function (response) {
                   $scope.cdss = response.data;
-                  $scope.districtss = 1;
-                  $scope.cdsss = null;
               });
               $http.get("/stock/casespalusDis/?report__facility__district=" + district.id)
               .then(function (response) {
+                  $scope.cdsss = false;
                   if (response.data.length > 0) {
                   $scope.structures = response.data;
                   }
@@ -58,16 +59,18 @@ app.controller('FilterCtrl', ['$scope', '$http', 'DTOptionsBuilder',  function($
         // CDS
         $scope.update_cds = function () {
             var cds = $scope.cds;
-            $scope.districtss = 1;
+            $scope.districtss = true;
+            $scope.cdsss = true;
             if (cds) {
               $http.get("/stock/casespalusCds/?report__facility=" + cds.id)
               .then(function (response) {
                   if (response.data.length > 0) {
-                  $scope.cdsss = 1;
                   $scope.structures = response.data;
                   } else  $scope.structures = {};
               });
       }
     };
+    // for export
+    $scope.dtOptions = DTOptionsBuilder.newOptions().withPaginationType('full_numbers').withButtons([ 'copy', 'csv', 'excel', 'pdf', 'print']).withDOM("<'row'<'col-sm-3'l><'col-sm-4'i><'col-sm-5'f>>" + "<'row'<'col-sm-12'tr>>" + "<'row'<'col-sm-4'B><'col-sm-8'p>>").withDisplayLength(10);  
   }]);
 
